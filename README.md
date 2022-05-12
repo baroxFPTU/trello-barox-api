cdb6751c2dea9a7941bff61df79cd59ff2c2fe7c
Bao Phan <pqb.barox@gmail.com>
2022-05-12 08:58:59 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# API for clone Trello app
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Information
+Author: Barox Phan
+Mentor: Trung Quan Dev
+Created at: 2022 05 12
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const hostName = 'localhost'
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.end('<h1>Hello world!</h1>')
+})
+
+app.listen(port, hostName, () => {
+  console.log(`I'm a server running at http://${hostName}:${port}`)
+})
