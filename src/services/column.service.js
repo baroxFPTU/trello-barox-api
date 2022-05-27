e50f51c9a99ab9b8380f6f0e469e685214b86184
unknown <pqb.barox@gmail.com>
2022-05-27 08:22:57 +0700

    handle CRUD column & card

--- src/services/column.service.js
+++ src/services/column.service.js
@@ -1,9 +1,11 @@
 import { ColumnModel } from '*/models/column.model'
 import { BoardModel } from '*/models/board.model'
+import { CardModel } from '../models/card.model'
 
 const createNew = async (column) => {
   try {
     const newColumn = await ColumnModel.createNew(column)
+    newColumn.cards = []
     await BoardModel.pushColumnOrder(newColumn.boardId.toString(), newColumn._id.toString())
 
     return newColumn
@@ -18,7 +20,16 @@ const update = async (id, column) => {
       ...column,
       updatedAt: Date.now()
     }
+
+    if (updatedColumn._id) delete updatedColumn._id
+    if (updatedColumn.cards) delete updatedColumn.cards
+
     const result = await ColumnModel.update(id, updatedColumn)
+
+    if (result._destroy) {
+      await CardModel.softDeleteCards(result.cardOrder)
+    }
+
     return result
   } catch (error) {
     throw new Error(error)
